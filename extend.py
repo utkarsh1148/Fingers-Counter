731f59a9705ebaf9145c942bf6a3b36771bbadde
utkarsh1148 <utkarsh1148@gmail.com>
2019-05-18 23:07:33 +0530

    updated

---
+++ extend.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+import copy
+import math
+from hand import cnt;
+
+if(cnt==0):
+	print("Either Fist closed or 1 finger")
+if(cnt==1):
+	print("2 fingers")
+if(cnt==2):
+	print("3 fingers")
+if (cnt==3):
+	print("4 fingers")
+if(cnt==4):
+	print("5 fingers")
+if(cnt==5):
+        print("6 fingers")
